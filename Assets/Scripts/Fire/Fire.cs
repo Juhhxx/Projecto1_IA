1da5216c91dc9406caaacb483ccc557255c2c47f
notCroptu <martinsmariana1972@gmail.com>
2025-04-25 00:31:18 +0100

    fire gpu instancing

--- Assets/Scripts/Fire/Fire.cs
+++ Assets/Scripts/Fire/Fire.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Profiling;
 
 namespace Scripts.Fire
 {
@@ -8,6 +9,7 @@ namespace Scripts.Fire
 
         [field:SerializeField] public long PolyRef { get; private set; }
         [field:SerializeField] public long[] NeiRefs { get; private set; }
+        [field:SerializeField] public Matrix4x4 Matrix { get; private set; }
         [SerializeField] private float _firePropagation = 0.005f;
 
         private float _lifeTime;
@@ -17,8 +19,9 @@ namespace Scripts.Fire
             _lifeTime = 0f;
         }
 
-        public void OrderedUpdate()
+        public void UpdateOrdered()
         {
+            Profiler.BeginSample("DRC Fire");
             _lifeTime += Time.deltaTime;
             if ( _lifeTime >= _duration )
                 gameObject.SetActive(false);
@@ -30,6 +33,7 @@ namespace Scripts.Fire
         #if UNITY_EDITOR
         public void SetRefs(long selfRef, long[] neiRefs)
         {
+           Matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
             PolyRef = selfRef;
             NeiRefs = neiRefs;
         }
