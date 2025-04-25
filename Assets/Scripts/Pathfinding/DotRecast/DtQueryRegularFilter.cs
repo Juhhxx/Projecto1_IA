35622e9916fe473f3fb3197fc0f71827e0a6fcf6
notCroptu <martinsmariana1972@gmail.com>
2025-04-25 22:28:32 +0100

    More main setup, add obstacle indicators to geometry and weights

--- Assets/Scripts/Pathfinding/DotRecast/DtQueryRegularFilter.cs
+++ Assets/Scripts/Pathfinding/DotRecast/DtQueryRegularFilter.cs
@@ -17,12 +17,12 @@ namespace Scripts.Pathfinding.DotRecast
         }
         public override float GetCost(RcVec3f pa, RcVec3f pb, long prevRef, DtMeshTile prevTile, DtPoly prevPoly, long curRef, DtMeshTile curTile, DtPoly curPoly, long nextRef, DtMeshTile nextTile, DtPoly nextPoly)
         {
-            float value = RcVec3f.Distance(pa, pb) * _areaCost[curPoly.GetArea()]; // need to assign area weights later through this
+            float value = RcVec3f.Distance(pa, pb) * _areaCost[ curPoly.GetArea() ]; // need to assign area weights later through this
             
             if ( _explosion.PolyHasFire(curRef) )
                 value *= 10;
             
             return value; // basic cost in distance, perhaps we should have a mono behavior child of the interface of DtMeshTile so we manually set these costs?
-            }
+        }
     }
 }
